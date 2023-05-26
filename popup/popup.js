bf58960f3c3103edc02acb80e422b49f91e042e8
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2023-05-26 10:07:03 -0400

    Alert() modified date of post

--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,30 @@
-console.log('salcode popup.js');
+async function getCurrentTab() {
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  return tab;
+}
+
+async function func() {
+  try {
+    const restApiUrlForPost = document.querySelector('link[rel="alternate"][type="application/json"]')?.href;
+    if (! restApiUrlForPost) {
+      throw new Error('There is no WordPress REST API URL on this page');
+    }
+    const postData = await (await fetch(document.querySelector('link[rel="alternate"][type="application/json"]').href)).json();
+    const modified = postData.modified;
+    if (! modified) {
+      throw new Error('There is no modified value for this page');
+    }
+    alert (`Last modified ${modified}`);
+  } catch ( error ) {
+    alert(`${error.toString()}`);
+  }
+}
+
+(async () => {
+  // Run func() inside current tab.
+  const tab = await getCurrentTab();
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func
+  });
+})();
